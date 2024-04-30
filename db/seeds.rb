b32f4cf8be550b637ede10b78c839ba45c464e17
Mikiko9882 <teddy.0802.t@gmail.com>
2024-04-30 16:05:48 +0900

    teacherまで追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,42 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+3.times do
+  Admin.create!(
+    email: Faker::Internet.email,
+    password: "12345678"
+  )
+end
 
-Admin.create!(
-  email: "admin@example.com",
-  password: "12345678"
-)
+6.times do
+  School.create!(
+    name: Faker::Educator.secondary_school,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.subscriber_number(length: 10),
+    email: Faker::Internet.email,
+    website: Faker::Internet.url,
+    code: Faker::Alphanumeric.alphanumeric(number: 8, min_alpha: 3),
+    admin_id: Admin.pluck(:id).sample,
+  )
+end
+
+class_names = ["1組", "2組", "3組", "4組", "5組", "6組", "7組", "8組", "9組", "10組"]
+
+School.all.each do |school|
+  class_names.each do |class_name|
+    StudentClass.create!(
+      name: class_name,
+      school_id: school.id
+    )
+  end
+end
+
+30.times do
+  Teacher.create!(
+    name: Faker::Name.name,
+    login_id: Faker::Lorem.characters(number: 10, min_alpha: 4, min_numeric: 1),
+    password: '12345678', # 仮のパスワード
+    school_id: School.pluck(:id).sample, # 学校のIDは存在するIDからランダムに選択してください
+  )
+end
+
 
 Grade.find_or_create_by!(name: "1年")
 Grade.find_or_create_by!(name: "2年")
